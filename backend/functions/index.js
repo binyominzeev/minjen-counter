a14222928fd6f73f5e4717e79ba3d69304f006bf
binyominzeev <szvbinjomin@gmail.com>
2025-05-31 23:41:54 +0200

    firebase version correction

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,6 @@
 // Firebase Functions entry point
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
